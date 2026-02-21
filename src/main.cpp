d8b2f54c419a1bae667b0b3bc1f30fca5ab2ff1d
ansh-codr <anshloverao@gmail.com>
2026-02-21 21:54:06 +0530

    complete bidirectional servo sweep with logging

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 
 /**
  * @file main.cpp
- * @brief Simple servo sweep demonstration
+ * @brief Simple servo sweep demonstration for MG995 closed-loop axis control system.
  * @author ansh-codr
  * @date 2026-02-21
  */
@@ -19,6 +19,15 @@ void setup() {
 void loop() {
 
     for (int i = 0; i <= 180; i += 10) {
+        myServo.write(i);
+
+        Serial.print("Servo Angle: ");
+        Serial.println(i);
+
+        delay(500);
+    }
+
+    for (int i = 180; i >= 0; i -= 10) {
         myServo.write(i);
 
         Serial.print("Servo Angle: ");
